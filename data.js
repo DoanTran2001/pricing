deb373007a9b0a3a7ef66ff4191ad869ac2dfffc
DoanTran2001 <dadotran3@gmail.com>
2023-02-13 16:25:32 +0700

    slider

---
+++ data.js
@@ -0,0 +1,39 @@
+const slider1 = [
+  {
+    image: './images/ocean.png',
+    name: 'Ocean',
+    price: '$599',
+  },
+  {
+    image: './images/mountain.png',
+    name: 'Mountains',
+    price: '$525',
+    title: 'The best price/quantity plans',
+    new: true
+  },
+  {
+    image: './images/forest.png',
+    name: 'Forest',
+    price: '$599',
+  },
+  {
+    image: './images/ocean.png',
+    name: 'Ocean 1',
+    price: '$599',
+  },
+  {
+    image: './images/mountain.png',
+    name: 'Mountains 1',
+    price: '$599',
+  },
+  {
+    image: './images/mountain.png',
+    name: 'Mountains 1',
+    price: '$599',
+  },
+  {
+    image: './images/mountain.png',
+    name: 'Mountains 1',
+    price: '$599',
+  },
+]
